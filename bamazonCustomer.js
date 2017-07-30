28560f65ca1815bd8be1a4079a51b16990691dc0
Christopher Le <christopherle@Christophers-MacBook-Air.local>
2017-07-30 14:28:17 -0700

    third push

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,35 +19,80 @@ connection.connect(function(err) {
   displayItems()
   sellItem();
 });
-  // --------------------------------------------------------------
-function displayItems(){
-  connection.query("SELECT * FROM products", function(err, res){
-  if (err) throw err;
-  console.log("======================================================");
-  console.log("======================================================");
-  console.log("Welcome! I've got a little bit of everything. Take a look around.");
-  console.log("======================================================");
-  console.log("======================================================");
-  for (var i = 0; i < res.length; i++) {
-    console.log("Item #: " + [res[i].item_id ,
-                " Product: " + res[i].product_name ,
-                " Department: " + res[i].department_name ,
-                " Price: "  + "$" + res[i].price ,
-                " Quantity: " + res[i].stock_quantity]);
- console.log("------------------------------------------------------");
-        }
+// --------------------------------------------------------------
+function displayItems() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log("======================================================");
+    console.log("======================================================");
+    console.log("Welcome! I've got a little bit of everything. Take a look around.");
+    console.log("======================================================");
+    console.log("======================================================");
+    for (var i = 0; i < res.length; i++) {
+      console.log("Item #: " + [res[i].item_id,
+        " Product: " + res[i].product_name,
+        " Department: " + res[i].department_name,
+        " Price: " + "$" + res[i].price,
+        " Quantity: " + res[i].stock_quantity
+      ]);
+      console.log("------------------------------------------------------");
+    }
   });
-
 }
 
 var sellItem = function() {
-inquirer.prompt({
-  name:"item_id",
-  type:"list",
-  message: "Which item would you to buy?",
-  choices: ["Bread", "Apple", "Banana", "Pizza", "Gordon Ramsay Gourmet Burger", "Delorean", "Ducati Monster",
-  "Diamond Ring", "Gold Chain", "Flame Thrower"]
-}).then(function(answer) {
-  console.log(answer);
-})
-}
+    connection.query("SELECT * FROM products", function(err, res) {
+          if (err) throw err;
+          inquirer.prompt([{
+            name: "item_id",
+            type: "list",
+            message: "Which item would you to buy?",
+            choices: ["Bread", "Apple", "Banana", "Pizza", "Gordon Ramsay Gourmet Burger", "Delorean", "Ducati Monster",
+              "Diamond Ring", "Gold Chain", "Flame Thrower"
+            ]
+          }, {
+            name: "stock_quantity",
+            type: "input",
+            message: "How much do you need?",
+            validate: function(value) {
+              if (isNaN(value) == false) {
+                return true;
+              } else {
+                return false;
+              }
+            }
+          }]).then(function(answer) {
+
+              connection.query("SELECT * FROM products", function(err, res) {
+                  if (err) throw err;
+                  var selectedItem;
+                  for (var i = 0; i < res.length; i++) {
+                    if (res[i].product_name === answer.item_id) {
+                      selectedItem = res[i];
+                      // console.log(res);
+                      console.log(answer.item_id);
+                    }
+                  }
+                  if (selectedItem.answer > parseInt(answer.stock_quantity)) {
+                    connection.query("UPDATE products SET? WHERE?", [{
+                          stock_quantity: (selectedItem.stock_quantity - parseInt(answer.stock_quantity))
+                        },
+                        {
+                          item_id: selectedItem.item_id
+                        }
+                      ],
+                      function(err) {
+                        if (err) throw err;
+                        sellItem();
+                      })
+                    }
+                    else {
+                      console.log("Sorry... We're out!")
+                      console.log(res[i]);
+                      console.log(selectedItem);
+                      // sellItem();
+                    }
+                  });
+              });
+          });
+        }
